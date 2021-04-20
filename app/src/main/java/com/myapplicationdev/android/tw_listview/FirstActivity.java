b20c6f752f79c9070e5d228294b72bf261fb9244
19020125 <19020125@myrp.edu.sg>
2021-04-20 14:29:18 +0800

    Intent which passes string (of which year) done. Intent received at second activity done.

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,9 +1,14 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,10 +28,21 @@ public class FirstActivity extends AppCompatActivity {
         al = new ArrayList<String>();
         al.add("Year 1");
         al.add("Year 2");
-        al.add("Year 2");
+        al.add("Year 3");
 
         aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedYear = al.get(position);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("year", selectedYear);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -6,19 +6,31 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
-
+    ArrayList<Module> alYear1;
+    ArrayList<Module> alYear2;
+    ArrayList<Module> alYear3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-//        lv = (ListView) this.findViewById(R.id.lvModules);
+        alYear1 = new ArrayList<Module>();
+        alYear2 = new ArrayList<Module>();
+        alYear3 = new ArrayList<Module>();
+
+        lv = (ListView) this.findViewById(R.id.lvModules);
         tvYear = (TextView) findViewById(R.id.tvYear);
+
+        alYear2.add(new Module("C208","Computer"));
+        alYear2.add(new Module("C200","Tree"));
+        alYear2.add(new Module("C346","Computer"));
 
         Intent i = getIntent();
         String year = i.getStringExtra("year");
