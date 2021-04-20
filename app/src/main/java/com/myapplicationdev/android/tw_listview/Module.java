14560c98249c1ea55e0ab770af436abd2c88ecb2
19020125 <19020125@myrp.edu.sg>
2021-04-20 13:11:41 +0800

    Class Module done

---
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Module.java
@@ -0,0 +1,16 @@
+package com.myapplicationdev.android.tw_listview;
+
+public class Module {
+    private String modName;
+    private String icon;
+    public Module(String modName, String icon) {
+        this.modName = modName;
+        this.icon = icon;
+    }
+    public String getModName() {
+        return modName;
+    }
+    public String getIcon() {
+        return icon;
+    }
+}
